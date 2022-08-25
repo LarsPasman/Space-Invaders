2050e1ae884ce680d96ae8b13e8697a1a456f240
Lars Pasman <95089151+LarsPasman@users.noreply.github.com>
2022-08-25 16:54:00 +0000

    Player input en muren stoppen

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ var lives = 3;
 var player = {
   x: 400,
   y: 700,
-  
+  speed: 8,
+  width: 60,
 }
 
 function setup() {
@@ -14,9 +15,11 @@ function setup() {
 function draw(){
   background (0);
   drawPlayer();
+  playerInput();
   drawUI();
 }
 
+//De speler UI tekenen (score en levens)
 function drawUI(){
  fill(255,255,255);
  stroke(30,150,30);
@@ -28,6 +31,7 @@ function drawUI(){
  text("LIVES: " + lives ,780 ,45);
 }
 
+//schietertje tekenen
 function drawPlayer(){
   fill(80,255,75);
   noStroke();
@@ -35,10 +39,23 @@ function drawPlayer(){
   fill(50,205,50);
   rect(player.x, player.y - 15, -15, 15,);
   fill(80,255,75)
-  rect(player.x,player.y,60,30,30);
+  rect(player.x,player.y,player.width,30,30);
 }
-
+//player input
 function playerInput(){
-
+ if (keyIsDown (LEFT_ARROW)){
+  player.x -= player.speed;  
+}
+ else if (keyIsDown(RIGHT_ARROW)){
+  player.x += player.speed;    
+  }
   
+//Player kan niet door de Linker muur heen
+  if (player.x - player.width/2 < 0 ){
+    player.x = player.width/2;
+  }
+//Player kan niet door de rechter muur heen
+  else if (player.x + player.width/2 > width){
+    player.x = width - player.width/2
+  }
 }
