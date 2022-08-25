e34d788a5594a56ef4d1000a4b988c1e872eb50a
Lars Pasman <95089151+LarsPasman@users.noreply.github.com>
2022-08-25 14:37:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var score = 0;
+var lives = 3;
+
+function setup() {
+  createCanvas(800,800);
+}
+
+function draw(){
+  background (0);
+  drawUI();
+}
+
+function drawUI(){
+ fill(255,255,255);
+ stroke(30,150,30);
+ strokeWeight(5);
+ textSize(30)
+ textAlign(LEFT);
+ text("SCORE: " + score ,28 ,45);
+ textAlign(RIGHT);
+ text("LIVES: " + lives ,780 ,45);
+}
